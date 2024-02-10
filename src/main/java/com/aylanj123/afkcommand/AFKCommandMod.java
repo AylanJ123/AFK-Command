7d15a35ae6820043735fbd2b12a5f137ab097fa4
AylanJ123 <aylanjosue@gmail.com>
2024-02-10 01:51:08 -0600

    Innit

---
+++ src/main/java/com/aylanj123/afkcommand/AFKCommandMod.java
@@ -0,0 +1,35 @@
+package com.aylanj123.afkcommand;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(AFKCommandMod.MODID)
+public class AFKCommandMod
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "afk_command";
+    // Directly reference a slf4j logger
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AFKCommandMod()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        LOGGER.info("Setting up AFK Command...");
+    }
+
+}
---
+++ src/main/java/com/aylanj123/afkcommand/Config.java
@@ -0,0 +1,41 @@
+package com.aylanj123.afkcommand;
+
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.config.ModConfigEvent;
+
+// An example config class. This is not required, but it's a good idea to have one to keep your config organized.
+// Demonstrates how to use Forge's config APIs
+@Mod.EventBusSubscriber(modid = AFKCommandMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class Config
+{
+    private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+
+//     public static final ForgeConfigSpec.ConfigValue<String> MAGIC_NUMBER_INTRODUCTION = BUILDER
+//             .comment("What you want the introduction message to be for the magic number")
+//             .define("magicNumberIntroduction", "The magic number is... ");
+
+//     private static final ForgeConfigSpec.ConfigValue<List<? extends String>> ITEM_STRINGS = BUILDER
+//             .comment("A list of items to log on common setup.")
+//             .defineListAllowEmpty("items", List.of("minecraft:iron_ingot"), Config::validateItemName);
+
+    static final ForgeConfigSpec SPEC = BUILDER.build();
+
+//     public static String magicNumberIntroduction;
+//     public static Set<Item> items;
+
+//     private static boolean validateItemName(final Object obj)
+//     {
+//         return obj instanceof final String itemName && ForgeRegistries.ITEMS.containsKey(new ResourceLocation(itemName));
+//     }
+
+    @SubscribeEvent
+    static void onLoad(final ModConfigEvent event)
+    {
+        // magicNumberIntroduction = MAGIC_NUMBER_INTRODUCTION.get();
+        // items = ITEM_STRINGS.get().stream()
+        //         .map(itemName -> ForgeRegistries.ITEMS.getValue(new ResourceLocation(itemName)))
+        //         .collect(Collectors.toSet());
+    }
+}
